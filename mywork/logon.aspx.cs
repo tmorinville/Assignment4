fab85777d1963d8350ab1a2df3b9ebcb797da110
Taya Morinville <taya.morinville@ndus.edu>
2023-11-08 18:20:03 -0600

    Added KarateSchool database and started on login page.

--- mywork/logon.aspx.cs
+++ mywork/logon.aspx.cs
@@ -13,5 +13,20 @@ namespace Assignment4.mywork
         {
 
         }
+
+        KarateSchoolDataContext dbcon;
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            // Store username and password entered by user
+            string username = Login1.UserName;
+            string password = Login1.Password;
+
+            // Connect to dbcon
+            string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\tmmor\\OneDrive\\Desktop\\Modern Software Development\\Assignment4\\App_Data\\KarateSchool(1).mdf\";Integrated Security=True;Connect Timeout=30";
+            dbcon = new KarateSchoolDataContext(connString);
+
+            // LINQ
+        }
     }
 }
